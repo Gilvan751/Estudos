58f47076d6ae6ee3695376e14e692f2347989239
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-10 13:22:45 -0300

    Valor transformado em dolar e ienne

---
+++ valorCarteira.py
@@ -0,0 +1,4 @@
+n =  float(input('Digite o valor que você tem na carteira: R$ '))
+d = n / 3.27
+y = n /0.049
+print('O valor que você pode comprar em dolares é:  U$$ {:.2f} em iene {:.2f}'.format(d ,y))
