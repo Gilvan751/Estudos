92048360662aef20699baefa5afb2edffe4c765b
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-12 09:20:03 -0300

    Tratamento de dados

--- playmusica.py
+++ playmusica.py
@@ -3,3 +3,4 @@ pygame.init()
 pygame.mixer.music.load('ex022.mp3')
 pygame.mixer.music.play()
 para = input('Digite algo para parar')
+
---
+++ tratamento.py
@@ -0,0 +1,8 @@
+salary_dict = {
+    'Junior Data Scientist': 80,
+    'Senior Data Scientist': 120,
+    'Lead Data Engineer': 140,
+}
+
+for employee, salary in salary_dict.items():
+    print(f'Position: {employee}, Salary: ${salary}000')
