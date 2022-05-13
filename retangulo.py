08e38b304d208d790c3e5c05884f3254f6f97575
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-13 15:07:36 -0300

    Area de um retangulo

---
+++ retangulo.py
@@ -0,0 +1,9 @@
+# Exercício 01 - área do retângulo
+
+print("Calcule a area de um retangulo")
+
+base = float(input("Qual o tamanho da base do seu retangulo em metros?   "))
+altura = float(input("Qual o tamanho da altura do seu retangulo em metros?  "))
+area = base * altura
+
+print(f'O seu retangulo de {base:.1f} m  de base por {altura:.1f} m de altura possui area de : {area:.1f}  m²')
