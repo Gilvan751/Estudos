1491db714cf5acec191684f8bf87fde0b7f4b36f
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-13 11:36:11 -0300

    Horario do dia

---
+++ horario.py
@@ -0,0 +1,8 @@
+horario = input('digite se é dia, tarde ou noite ')
+if horario == 'dia':
+    print('O sol esta quente')
+elif horario == 'tarde':
+    print('O por do sol esta lindo')
+    print('Tenha uma boa tarde')
+else:
+    print('já e noite')
