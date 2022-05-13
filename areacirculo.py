257303c85e47d10c13d909202821855924c6c20c
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-13 15:28:04 -0300

    Calculo da área de um círculo

---
+++ areacirculo.py
@@ -0,0 +1,6 @@
+#04 - área de um círculo - pi = 3,141592
+print('Área de um círuclo')
+r = float(input('Entre com o valor do raio do circulo em centimetros '))
+pi = 3.141592
+a = pi* (r**2) 
+print(f'Um círculo de {r:.1f} cm raio, tem como área o valor de {a:.1f} cm³')
