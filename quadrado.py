4e4b51d56e5e80516fefb4f9032760d13085503c
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-13 15:14:13 -0300

    Calcular a area de um quadrado

---
+++ quadrado.py
@@ -0,0 +1,5 @@
+#02 - área de um quadrado
+print('Área de um quadrado ')
+l = float(input('Digite o valor do lado de seu quadrado em metros? '))
+area = l * l
+print(f'Seu quadrado de lado {l:.1f} metros tem área de {area:.1f} m²')
