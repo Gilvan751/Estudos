0d6425658c09adc52913e8b54fcb2ecf1cc37319
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-14 21:05:23 -0300

    comparação entre numeros digitados

---
+++ comparacaonumeros.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+if n1 > n2:
+    print('O primeiro numero é MAIOR')
+elif n2 > n1:
+    print('O maior numero é o SEGUNDO')
+else:
+   print('Os dois numeros são IGUAIS')
