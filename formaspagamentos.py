17173ebbcd5a46eefd0fec06305c9858dd1eb9e2
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-15 10:25:43 -0300

    Formas de pagamento com desconto

---
+++ formaspagamentos.py
@@ -0,0 +1,29 @@
+print('{:=^40}'.format(' LOJAS GUANABARA '))
+preco = float(input('Preço da COMPRA: R$ '))
+print(''' FORMA DE PAGAMENTO 
+[ 1 ] à vista dinheiro / cheque
+[ 2 ] à vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opcao = int(input('Qual é a opção? '))
+if opcao == 1:
+    total = preco - (preco * 10 / 100)
+elif opcao == 2:
+    total = preco - (preco * 5 / 100)
+elif opcao == 3:
+    total = preco
+    parcela = total / 2
+    print(
+        'Sua compra será parcelada 2x de R$ {:.2f} SEM JUROS'.format(parcela))
+elif opcao == 4:
+    total = preco + (preco * 20 / 100)
+    totparc = int(input('Quantas parcelas ? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R$ {:.2f} no final COM JUROS.'.format(
+        totparc, parcela))
+
+else:
+    total = preco
+    print('\033[1;31mOPÇÃO INVÁLIDA de pagamento. Tente novamente!')
+print('Sua COMPRA de R$ {:.2f} vai custar R$ {:.2f} no fianl'.format(
+    preco, total))
