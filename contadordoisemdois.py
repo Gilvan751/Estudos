9b4294bf03d6c92904b7a4d035dd5a48580ae102
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-18 11:55:44 -0300

    Laço for para contar de odis em dois

---
+++ contadordoisemdois.py
@@ -0,0 +1,3 @@
+for cont in range(2, 52, 2):
+    print('\t', cont)
+print('ACABOU')
--- lacorepeticao.py
+++ lacorepeticao.py
@@ -1,5 +1,5 @@
 from time import sleep
 for cont in range(10, -1, -1):
-    print('\t', cont)
+    print('\t',cont)
     sleep(1)
 print('ACABOU! BUM! POOOW!')
