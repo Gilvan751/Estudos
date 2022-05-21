b2b6e740d833cbd3631234288eb8b259a1930773
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-21 15:22:56 -0300

    utilização do comando senão

---
+++ valorzero.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número: "))
+if num == 0:
+    print("O número que você digitou foi zero")
+else:
+    print(f"O número que você digitou foi {num} o qual é diferente de zero")
