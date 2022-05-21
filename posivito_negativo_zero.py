a9bdb6ce1f4fe27316d5a3494526c69661e7bd7a
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-21 19:58:19 -0300

    Saber se um valor digitado e positivo ou negativo ou zero

---
+++ posivito_negativo_zero.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um número inteiro: "))
+if (num == 0):
+    print(f"O valor digitado é igual a {num}")
+elif (num > 0):
+    print(f"O valor digitado é um valor positivo")
+else:
+    print(f"O valor digitado é um valor negativo")
