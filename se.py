6e9040627369515263e231045d2f56a19d6bd27f
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-21 15:00:51 -0300

    Utilização docomando se

---
+++ se.py
@@ -0,0 +1,8 @@
+numero = int((input("Digite um numero: ")))
+if numero> 18:
+    
+    print(f'E maior de idade, você tem {numero} anos pode entrar')
+else:
+        
+     print(f"E menor de idade pois tem {numero} anos, ainda não pode entrar")
+        
