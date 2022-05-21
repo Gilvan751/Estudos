f7f812592c2816890c8635eb2b0ccc5c0ad5cdf4
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-21 20:36:07 -0300

    Comparação entre três números digitados

---
+++ mior_tres_numeros.py
@@ -0,0 +1,14 @@
+
+n1 = int(input("Digite o primeiro numero inteiro "))
+n2 = int(input("Digite o primeiro numero inteiro "))
+n3 = int(input("Digite o primeiro numero inteiro "))
+if (n1 > n2 and n1 > n3):
+    print(" O primeiro numero digitado é o maior\n")
+elif(n2 > n1 and n2 > n3): 
+    print("O segundo numero digitado é o maior\n")
+elif (n3 > n1 and n3 > n2):
+    print("o terceiro numero digitado é o maior\n")
+
+
+
+
