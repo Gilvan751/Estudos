33f071c01524cb4249869537bae5fd5524d144d8
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-24 10:09:08 -0300

    Propriedades de uma string

---
+++ estudo_strings.py
@@ -0,0 +1,13 @@
+#tamanho propriedades
+l = 'lista de letras'
+data = '24/05/2022'
+
+tamanho = len(l)
+print(tamanho)
+#split da string serve para dividir intervalos que vão utilizar
+lista = l.split(" ")
+lista2 = data.split('/')
+print(lista)
+print(lista2)
+#substituição substituir um espaço ou qualquer palavra por outra replace
+print(l.replace("de", ""))
