27acc84ed0f1b5c80c0fd8f037ffd14b5a7eff49
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-24 10:30:28 -0300

    Formas de percorrer uma string com o comando for e while

--- estudo_strings.py
+++ estudo_strings.py
@@ -1,25 +1,40 @@
 #tamanho propriedades
-l = 'lista de letras'
+''' l = 'lista de letras'
 data = '24/05/2022'
 
 tamanho = len(l)
-print(tamanho)
+print(tamanho) '''
 #split da string serve para dividir intervalos que vão utilizar
-lista = l.split(" ")
+''' lista = l.split(" ")
 lista2 = data.split('/')
 print(lista)
-print(lista2)
+print(lista2) '''
 #substituição substituir um espaço ou qualquer palavra por outra replace
-print(l.replace("de",""))
+''' print(l.replace("de","")) '''
 #fatiamento e divisão de string[0] [1:3] [1:2:4]
-nome = 'python'
+''' nome = 'python'
 nova_string = nome[4]
 print(nova_string)
 #ASCII
 print('a' == 'b')
 print('b' != 'a')
 print('a' > 'X')
-print('a' > '1')
+print('a' > '1') '''
 
-for i in range(122):
-    print(str(i) + " - "+chr(i))
+''' for i in range(122):
+    print(str(i) + " - "+chr(i)) '''
+
+nome = 'python'
+#percorrer com o for
+for i in nome:
+    print(i)
+print('=-=-=0=0=0=0=0=0=0')
+#percorrer com o while
+i = 0
+while i < len(nome):
+    print(nome[i])
+    i+=1
+print('=-=-=0=0=0=0=0=0=0')
+#percorrer com for / enumerate
+for k, v in enumerate(nome):
+    print(k,v)
