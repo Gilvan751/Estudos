2c8d2541ba1f0218c6411da110d64ed1a81abaa9
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-24 10:23:18 -0300

    Fatiamento e tabela ASCII

--- estudo_strings.py
+++ estudo_strings.py
@@ -10,4 +10,16 @@ lista2 = data.split('/')
 print(lista)
 print(lista2)
 #substituição substituir um espaço ou qualquer palavra por outra replace
-print(l.replace("de", ""))
+print(l.replace("de",""))
+#fatiamento e divisão de string[0] [1:3] [1:2:4]
+nome = 'python'
+nova_string = nome[4]
+print(nova_string)
+#ASCII
+print('a' == 'b')
+print('b' != 'a')
+print('a' > 'X')
+print('a' > '1')
+
+for i in range(122):
+    print(str(i) + " - "+chr(i))
