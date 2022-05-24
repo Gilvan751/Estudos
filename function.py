80babcc3f323ff2d788708929ed230adfeed8222
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-24 19:50:35 -0300

    Construção com funções

---
+++ function.py
@@ -0,0 +1,32 @@
+''' from tkinter import Y
+
+
+def funcao():
+    print("Eu sou uma função")
+funcao() '''
+
+''' def somar(x , y):
+    total = x + y
+    print(f'o tatal da soma entre os numeros {x} + {y} = {total}')
+
+somar(12,25) '''  
+''' def show():
+    return 15
+
+print(show()) '''
+
+''' def mult(a,b):
+    return a*b
+
+print(mult(5,6)) '''
+
+def calc(a,b):
+   s = a + b
+   m = a * b
+   d = a / b
+   n = a - b
+   print(s)
+   print(m)
+   print(d)
+   print(n)
+calc(10,2)
