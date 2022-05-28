e3a52cbf3585c9bb2da1ba923816472536e9e948
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-28 16:10:24 -0300

    Digitar um valor e dizer se o mesmo é posivito ou negativo

---
+++ negativo_positivo.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número positivo ou negativo? '))
+if (num < 0):
+    print(f'Você digitou o número {num} o qual é negativo')
+else:
+    print(f'Você digitou o número {num} o qual é positivo')
