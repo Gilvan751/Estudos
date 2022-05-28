f0d1bb8db78aa7c93ebd7abf61fa487f60c5d59d
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-28 16:18:32 -0300

    Identificação se o sexo e Masculino ou feminino

---
+++ identidade_ideologica.py
@@ -0,0 +1,6 @@
+num = str(input('Digite a letra [M ou F] para saber qual o sexo digitado: ')).upper().strip()[0]
+if num == 'M':
+    print('Você digitou ""M" o qual se refere ao sexo Masculino')
+
+else:
+    print('Você digitou "F" o qual se refere ao sexo Feminino')
