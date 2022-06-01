80ee48919e4b16e9527156dcd52db124968d9872
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-01 16:24:18 -0300

    Utilização direta de uma função

--- adicionar_15.py
+++ adicionar_15.py
@@ -1,10 +1,13 @@
-m = int(input('Digite um valor que você conseguiu durante a semana: '))
-adiciona_15 = m
-if m >= 100:
-    m +=15
-    print(f'o valor de {m}') 
-else:
-    print(f'Guarde mais dinheiro Pedro')
+#m = int(input('Digite um valor que você conseguiu durante a semana: '))
+def adiciona_15 (m):
+    if m >= 100:
+        m +=15
+        print(f'o valor de {m}') 
+    else:
+        print(f'Guarde mais dinheiro Pedro')
+        
+        
+adiciona_15(120)
 
 
         
