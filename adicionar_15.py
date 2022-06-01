9fd883db40aa8696fc99a2a854c545af99cd2c1b
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-01 16:03:35 -0300

    Calculo de 15 reais por semana se conseguir acumular valor acima de 100 reais

---
+++ adicionar_15.py
@@ -0,0 +1,10 @@
+m = int(input('Digite um valor que você conseguiu durante a semana: '))
+adiciona_15 = m
+if m >= 100:
+    m +=15
+    print(f'o valor de {m}') 
+else:
+    print(f'Guarde mais dinheiro Pedro')
+
+
+        
