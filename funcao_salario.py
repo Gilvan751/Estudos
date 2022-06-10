243b5210119e5aa0d812a6fb9251607c70f0ba93
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-10 12:51:52 -0300

    Função dentro de função para atualizar valor com bonus

---
+++ funcao_salario.py
@@ -0,0 +1,6 @@
+def salario(hrs_traba):
+    return hrs_traba * 40
+
+def cont_bonus(hrs_traba):
+    return salario(hrs_traba) + 50
+print(salario(8), cont_bonus(8))
--- functionnativa.py
+++ functionnativa.py
@@ -1,7 +1,14 @@
 agenda = (1,2,3,4)
-sum(agenda)
 print(sum(agenda))
+a = 5.43269
+print(round(a,4))
+b = (12, 19, 2, 79,250)
+print(min(b))
+print(max(b))
+print(len(b))
+c = 5
+print(pow(c,4))
 
-round(5.43269,2)
+
 
 
