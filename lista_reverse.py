343438ac8a8180b904e9a2863be293d152919936
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-10 18:50:49 -0300

    Utilização do sort e revertse para mudar a ordem da lista

---
+++ lista_reverse.py
@@ -0,0 +1,8 @@
+participante = [1,2,3,4,5,6]
+novos = [10,20,30,40,50,60,70,80,90]
+lista_geral = [participante, novos]
+novos.sort(reverse=True)
+participante.sort(reverse=True)
+print(participante)
+print(novos)
+print(lista_geral)
