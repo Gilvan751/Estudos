0f39a558471b6d6e34aed9fd3e29da9dd40bc58d
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-10 18:23:38 -0300

    Utilização de funções nativas para saber o primeiro e o ultimo elemento de uma lista

--- aplicacao_lista_estudos.py
+++ aplicacao_lista_estudos.py
@@ -1,4 +1,13 @@
 convidados = ['pedro', 'jose', 'maria', 'monica']
 convidados[1] = 'Manarino'
 del convidados[-1]
+convidados.append('Marta')
+convidados.append('Joselia')
+convidados.append('Martineide')
+convidados.remove('Marta')
+convidados.extend(['jose', 'manoel', 'Rubia', 'Talisma'])
+lista = len(convidados) - 1
+print(lista)
 print(convidados)
+print(f'O primeiro participante é : {convidados[0]}')
+print(f'O ultimo participante é : {convidados[8]}')
