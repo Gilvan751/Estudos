da8b1f10986cadfbe8f2e8af78ea100686007496
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-12 10:13:49 -0300

    Estudos de funções e tuplas

--- area_retangulo.py
+++ area_retangulo.py
@@ -5,3 +5,13 @@ def area_retangulo(x,y):
     return A, P
 
 print(area_retangulo(3,5))
+
+x = (12,13,14)
+y = (45,46,47)
+lista = [x,y]
+print(lista)
+
+nome,idade = "Gilvan, 50".split(',')
+print(nome)
+print(idade)
+print(x[2])
