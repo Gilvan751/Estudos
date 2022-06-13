e2abbd2b2694d9c22c0c05ad3d98694c834e9bae
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-13 05:32:56 -0300

    Draw circle

---
+++ draw_circle.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.Screen()
+t= turtle.Turtle()
+
+t.speed(0)
+t.pensize(2)
+
+colors = ['red', 'green', 'orange', 'violet',
+          'yellow', 'blue']
+for i in range(180):
+    t.color(colors[180 % 6])
+    t.fd(100)
+    t.rt(30)
+    t.fd(20)
+    t.lt(60)
+    t.fd(50)
+    t.rt(30)
+    
+    t.penup()
+    t.setposition(0,0)
+    t.pendown()
+    t.rt(2)
+    
+turtle.done()
