08127b5afa96523dedb2df5fd21400ca85084074
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-14 10:33:39 -0300

    Utilização de intrução condicional em uma função range

---
+++ Instrucao_condicional_loops.py
@@ -0,0 +1,10 @@
+print(list(range(11)))
+
+for n in list(range(11)):
+    print(2**n, end=' ')
+print('=-='*10)   
+for x in list(range(21)):
+    if x % 2 == 0:
+        print(x, end=' ')
+    else:
+        print('impar ', end= ' ')
