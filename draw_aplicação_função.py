510e3e4d5e63a0bf7cd3c39b9964f975328148e9
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-16 11:28:34 -0300

    Utilização de uma função de fora para dentro

--- draw_aplicação_função.py
+++ draw_aplicação_função.py
@@ -3,14 +3,16 @@ wn = turtle.Screen()
 wn.bgcolor("light green")
 wn.title("Turtle")
 skk=turtle.Turtle()
-skk.color("blue")
+skk.pensize(5)
+""" skk.color("blue") """
+colors = ['red', 'green', 'orange', 'violet']
 
 def sqrfunc(size):
 	for i in range(4):
+		skk.color(colors[i % 8])
 		skk.fd(size)
-		skk.left(90)        
-		size = size-5
-        
+		skk.lt(90)
+		size = size -5
 
 sqrfunc(146)
 sqrfunc(126)
@@ -19,4 +21,5 @@ sqrfunc(86)
 sqrfunc(66)
 sqrfunc(46)
 sqrfunc(26)
+
 turtle.done()
