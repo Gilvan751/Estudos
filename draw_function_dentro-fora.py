87b179d6f9434e3d3553291b5d7cb81cee7b85f3
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-16 11:30:46 -0300

    função de dentro para fora

---
+++ draw_function_dentro-fora.py
@@ -0,0 +1,25 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Turtle")
+skk=turtle.Turtle()
+skk.pensize(5)
+""" skk.color("blue") """
+colors = ['red', 'green', 'orange', 'violet']
+
+def sqrfunc(size):
+	for i in range(4):
+		skk.color(colors[i % 8])
+		skk.fd(size)
+		skk.lt(90)
+		size = size + 5
+sqrfunc(6)
+sqrfunc(26)
+sqrfunc(46)
+sqrfunc(66)
+sqrfunc(86)
+sqrfunc(106)
+sqrfunc(126)
+sqrfunc(146)
+
+turtle.done()
