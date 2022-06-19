5a772da45aa0b1918f5dbc5fbd557603fceb8d00
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-19 10:19:25 -0300

    Desenho de uma fecha com setas para os lados

---
+++ draw.circulo.py
@@ -0,0 +1,10 @@
+import turtle
+tela = turtle.Screen()
+
+turtle.home()
+turtle.dot()
+turtle.fd(150); turtle.dot(20, "blue"); turtle.fd(150); turtle.lt(90); turtle.fd(150)
+turtle.position()
+
+turtle.heading()
+turtle.done()
