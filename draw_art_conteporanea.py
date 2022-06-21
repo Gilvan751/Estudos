4e4771437323cff0a93dbea219a001c6b71d8e07
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-21 10:40:09 -0300

    troca de variaveis no sistema

--- draw_art_conteporanea.py
+++ draw_art_conteporanea.py
@@ -9,11 +9,12 @@ time.sleep(0)
 total = 5
 repeticao = 50
 t.fd(total)
+grau = 91
 colors = ['green', 'yellow','violet','blue']
 for i in range(repeticao):
     t.color(colors[i % 4])
     t.fd(total)
-    t.lt(91)
+    t.lt(grau)
     total +=10
 
 
