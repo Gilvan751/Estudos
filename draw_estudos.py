6d3dd5d3343f0fa3a7bad2c76034c34cbd95e2dd
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-28 22:24:43 -0300

    Esudo de uma nova função para aplicação

--- draw_estudos.py
+++ draw_estudos.py
@@ -1,6 +1,8 @@
 import turtle
 turtle.Screen()
 bob = turtle.Turtle('turtle')
+bob.speed(10)
+
 
 def square(bob):
     for i in range(4):
@@ -10,10 +12,28 @@ def square(bob):
 
 square(bob)
 alice = turtle.Turtle()
+alice.speed(10)
 alice.up()
 alice.goto(100,100)
 alice.pd()
 square(alice)
+
+def squares(t, length):
+    for i in range(4):
+        bob.fd(length)
+        bob.lt(90)
+
+squares(bob, 90)
+
+
+def polygon(t, n, length):
+     angle = 360 / n
+     for i in range(n):
+         t.fd(length)
+         t.lt(angle)
+
+polygon(bob, 7, 70)
+
 
 
 turtle.done()
