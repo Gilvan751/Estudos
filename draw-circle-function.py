3a6ee561c453e785dcb76a3bc8fad9b425f17034
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-10 19:01:59 -0300

    Função de diversas cirvulos

--- draw-circle-function.py
+++ draw-circle-function.py
@@ -1,8 +1,128 @@
-import turtle
-w = turtle.Screen()
+from turtle import *
+bgcolor('black')
+speed(0)
 
-t = turtle.Turtle()
-for i in range(4):
-    t.rt(90)
-    t.fd(50)
-turtle.done()
+
+penup()
+left(90)
+goto(0,0)
+left(180)
+pendown()
+j = 0
+
+for rings in range(2):
+    pencolor('red')
+    for i in range(150):
+        forward(100)
+        left(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+    pencolor('dark blue')
+    for i in range(150):
+        forward(102)
+        right(111)
+    penup()
+    right(180 * j)
+    pendown()
+
+    pencolor('dark violet')
+    for i in range(150):
+        forward(104)
+        left(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+    pencolor('dark red')
+    for i in range(150):
+        forward(106)
+        right(111)
+    penup()
+    right(180 * j)
+    pendown()
+
+    pencolor('dark orange')
+    for i in range(150):
+        forward(104)
+        left(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+    pencolor('dark blue')
+    for i in range(150):
+        forward(102)
+        right(111)
+    penup()
+    right(180 * j)
+    pendown()
+
+    pencolor('dark grey')
+    for i in range(150):
+        forward(100)
+        left(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+    pencolor('dark red')
+    for i in range(150):
+        forward(100)
+        right(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+    pencolor('dark blue')
+    for i in range(150):
+        forward(102)
+        left(111)
+    penup()
+    right(180 * j)
+    pendown()
+
+    pencolor('dark violet')
+    for i in range(150):
+        forward(104)
+        right(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+    pencolor('dark red')
+    for i in range(150):
+        forward(106)
+        left(111)
+    penup()
+    right(180 * j)
+    pendown()
+
+    pencolor('dark orange')
+    for i in range(150):
+        forward(104)
+        left(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+    pencolor('dark blue')
+    for i in range(150):
+        forward(102)
+        right(111)
+    penup()
+    right(180 * j)
+    pendown()
+
+    pencolor('dark grey')
+    for i in range(150):
+        forward(100)
+        left(111)
+    penup()
+    right(90 * j)
+    pendown()
+
+
+
+done()
