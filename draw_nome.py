000d5d68960937d6a6c9b710f5d8e25c17cdc19e
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-13 22:00:20 -0300

    Escrita do nome na tela

---
+++ draw_nome.py
@@ -0,0 +1,54 @@
+import turtle
+w = turtle.Screen()
+t = turtle.Turtle('turtle')
+#letraG
+t.goto(50,0)
+t.goto(50,-50)
+t.goto(-25,-50)
+t.goto(-25,50)
+t.goto(50,50)
+t.goto(50,20)
+t.up()
+t.goto(100,50)
+#letra I
+t.pd()
+t.goto(100,-50)
+t.up()
+t.goto(150,50)
+#letra L
+t.pd()
+t.goto(150,-50)
+t.goto(200,-50)
+t.up()
+t.goto(250,-50)
+#letra V
+t.pd()
+t.goto(225,50)
+t.goto(250,-50)
+t.goto(275,50)
+t.goto(250,-50)
+t.up()
+t.goto(300, -50)
+#letra A
+t.pd()
+t.goto(325,50)
+t.goto(350,-50)
+t.up()
+t.goto(312, 0)
+t.pd()
+t.goto(340,0)
+t.up()
+t.goto(400,-50)
+#Letra N
+t.pd()
+t.goto(400,50)
+t.goto(425,-50)
+t.goto(425,50)
+
+
+
+
+
+
+
+turtle.done()
