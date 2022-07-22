d31ad2cd7a41765f7faacb6b5590ec6d90eb4ce0
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-22 16:33:22 -0300

    Triangulo com logo

---
+++ draw_compra.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen()
+turtle.hideturtle()
+for i in range(3):
+    turtle.fd(150)
+    turtle.lt(120)
+for i in range(3):
+    turtle.fd(75)
+    turtle.lt(120)
+for i in range(3):
+    turtle.fd(150)
+    turtle.lt(120)
+    turtle.fd(75)
+turtle.done()
